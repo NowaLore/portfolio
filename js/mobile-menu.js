1fa22f376303ed0f3374491ca7c9c2296f61db02
Stam Manager <stammanager@gmail.com>
2024-06-02 16:50:02 +0300

    feat: added a function to close the mobile menu when clicking on the anchor link

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,10 +1,19 @@
-// 1. Создать якорные ссылки
-// 2. Реализовать функцию закрытя мобильного меню при клике на якорную ссылку
+// 1. Создать якорные ссылки +
+// 2. Реализовать функцию закрытия мобильного меню при клике на якорную ссылку
+// - Поставить прослушку события click на якорные ссылки
+// - Удалять класс open с меню
 // 3. Анимация кнопки бургера
 
 const burgerButton = document.querySelector("#burger");
 const menu = document.querySelector("#nav");
+const anchorLinks = document.querySelectorAll("[data-anchor]");
 
 burgerButton.addEventListener("click", function () {
     menu.classList.toggle("open");
-})
+});
+
+anchorLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("open");
+    })
+});
