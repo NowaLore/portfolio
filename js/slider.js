d7a6567c03eb38e04d9c5be5f6fc8f108aabffb6
Denis Titov <denisskysmart@gmail.com>
2024-08-04 17:50:54 +0300

    feat:added isCurrentButton function

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 const slider = document.querySelector("#slider");
 const slides = slider.querySelectorAll("[data-slide]");
 const sliderTrack = slider.querySelector("#track");
-const sliderButtonsControl = slider.querySelectorAll("[data-button]");
+const sliderButtonsControl = Array.from(slider.querySelectorAll("[data-button]"));
 const slideWidth = slides[0].offsetWidth;
 const slideGap = parseFloat(getComputedStyle(sliderTrack).columnGap);
 const currentDistance = slideWidth + slideGap;
@@ -22,6 +22,11 @@ const changeActiveStatus = () => {
 const hendlersEvents = (event) => {
     const isArrowRight = event.target.closest("[data-arrow-right]");
     const isArrowLeft = event.target.closest("[data-arrow-left]");
+    const isCurrentButton = event.target.closest("[data-button]");
+
+    if (isCurrentButton) {
+        counterSlide = sliderButtonsControl.indexOf(isCurrentButton);
+    }
     
     if (isArrowRight) {
         counterSlide = (counterSlide < slides.length - 1) ? counterSlide + 1 : 0;
