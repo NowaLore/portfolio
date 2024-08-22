008cf3a4ee617140635340814a6960688ab825e6
Denis Titov <denisskysmart@gmail.com>
2024-08-22 19:26:11 +0300

    feat: added animation for hero, avatar, about me and all card

--- js/animation.js
+++ js/animation.js
@@ -5,3 +5,50 @@ gsap.from('#hero',{
     opacity:0,
     ease:"bounce(1.5)",
 })
+
+gsap.from('#avatar',{
+    x:400,
+    duration:2,
+    opacity:0,
+    ease:"back.out(1.5)",
+})
+
+gsap.from('#about-me__content',{
+    opacity:0,
+    y:200,
+    scrollTrigger:{
+        trigger: '#about-me',
+        start: 'top 80%',
+        end:'65% 70%',
+        scrub: 2,
+        toggleActions: 'play none none reverse',
+    }
+})
+
+gsap.from('[data-skill-card]',{
+    stagger:0.2,
+    opacity:0,
+    y:200,
+    scrollTrigger:{
+        // markers:true,
+        trigger: '#skills',
+        start: 'top 70%',
+        end:'30% 50%',
+        scrub: 3,
+        toggleActions: 'play none none reverse',
+    }
+})
+
+gsap.from('[data-project-card]',{
+    stagger:0.2,
+    opacity:0,
+    y:200,
+    scrollTrigger:{
+        // markers:true,
+        trigger: '#projects',
+        start: 'top 40%',
+        end:'60% 70%',
+        scrub: 3,
+        toggleActions: 'play none none reverse',
+    }
+})
