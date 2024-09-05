880aae0feaf2cc5b718a89e8194b17b3b399b318
Denis Titov <denisskysmart@gmail.com>
2024-09-05 19:10:20 +0300

    feat: added block fade

--- js/card.js
+++ js/card.js
@@ -1,18 +1,18 @@
 const rowCard = document.querySelector("#row-card");
-// const fade = document.querySelector("#fade");
+const fade = document.querySelector("#fade");
 
 const hendlerAction = (event) => {
     const isButtonCard = event.target.closest("[data-skill-card]");
     if (isButtonCard) {
         isButtonCard.classList.add("active");
-        // fade.classList.add("active");
+        fade.classList.add("active");
     }
 
     const isButtonCross = event.target.closest("[data-btn-close]");
     if (isButtonCross) {
         const parentCard = isButtonCross.closest("[data-skill-card]");
         parentCard.classList.remove("active");
-        // fade.classList.remove("active");
+        fade.classList.remove("active");
     }
 }
 
