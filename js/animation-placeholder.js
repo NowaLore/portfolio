6ce95adc6b6c402871fd1218baac432f21babd8c
Stam Manager <stammanager@gmail.com>
2024-10-10 19:34:36 +0300

    feat:added blur function for placeholder

--- js/animation-placeholder.js
+++ js/animation-placeholder.js
@@ -5,7 +5,19 @@ const focusInput = (event) => {
     if (isInput) {
         const formItem = event.target.closest("[data-article]");
         formItem.classList.add("focus");
+        isInput.addEventListener("blur", blurInput);
     }
+}
+
+const blurInput = (event) => {
+    const formItem = event.target.closest("[data-article]");
+
+    const formInput = formItem.querySelector("[data-input]");
+    if (formInput.value.trim().length < 1) {
+        formItem.classList.remove("focus");
+    }
+
+    formInput.removeEventListener("blur", blurInput);
 }
 
 form.addEventListener("click", focusInput);
